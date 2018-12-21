fb91bc447009bef6832267fe67012453fcbfbcc1
baobaozhou <1030626864@qq.com>
2018-12-21 10:34:53 +0800

    add

---
+++ crf/test.py
@@ -0,0 +1,38 @@
+# def toRow():
+#     f1 = open('test111.txt', "r")
+#     f2 = open('data', "w")
+#
+#     line = list(f1.readline())
+#     n = 0
+#     while n < len(line):
+#         line[n] = line[n] + "\n"
+#         n = n + 1
+#     f2.writelines(line)
+#     f2.close()
+#     f1.close()
+# if __name__=="__main__":
+#     toRow()
+
+
+def toRow():
+    f1 = open('test111.txt', "r",encoding='utf-8')
+    f2 = open('data', "w",encoding='utf-8')
+    line=f1.readlines()
+    # # print(line[1])
+    # for line in f1.readlines():
+    #     char=list(line)
+    #
+    n=0
+    while n<len(line):
+        char=list(line[n])
+        m=0
+        while m<len(char):
+            char[m]=char[m]+"\n"
+            f2.write(char[m])
+            m=m+1
+        n=n+1
+    f2.close()
+    f1.close()
+if __name__ == "__main__":
+    toRow()
+
